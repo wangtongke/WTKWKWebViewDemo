018d3fbd35acfeeaa588eecb49c6f8d18781df7d
wangtk <wangtk@xh-invest.com>
2020-03-26 11:52:19 +0800

    init

---
+++ TKWKWebViewDemo/TKWKWebViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TKWKWebViewDemo
+//
+//  Created by 王同科 on 2020/3/26.
+//  Copyright © 2020 王同科. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TKWKWebViewDemo/TKWKWebViewDemoTests/TKWKWebViewDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  TKWKWebViewDemoTests.swift
+//  TKWKWebViewDemoTests
+//
+//  Created by 王同科 on 2020/3/26.
+//  Copyright © 2020 王同科. All rights reserved.
+//
+
+import XCTest
+@testable import TKWKWebViewDemo
+
+class TKWKWebViewDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
